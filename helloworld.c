7663917e844c3ad2ba4fec381dc140f0cc9a743e
princeimmanue <princeimmanue@gmail.com>
2023-04-12 00:02:00 -0700

    Displaying hello world using C

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+//Displaying hello world using C
+// code by Prince immanuel 
+#include<stdio.h>
+int main(){
+    printf(" Hello world !");
+    return 0 ;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+//Displaying hello world using c++
+// code by Prince immanuel 
+#include<iostream> 
+using namespace std;
+int main(){
+
+    cout<<"Hello World !";
+
+}
